cbacae8c5f7fa20310e9b83e4f14f473763db8c1
michelranari <32563252+michelranari@users.noreply.github.com>
2020-10-16 15:10:33 +0200

    Update database.sql
    change mysql srcipt to postgre script

--- database.sql
+++ database.sql
@@ -4,86 +4,90 @@
 
 CREATE DATABASE iwa;
 
-#------------------------------------------------------------
-# Table: User
-#------------------------------------------------------------
+------------------------------------------------------------
+--        Script Postgre 
+------------------------------------------------------------
 
+
+
+------------------------------------------------------------
+-- Table: User
+------------------------------------------------------------
 CREATE TABLE iwa.User(
-        id_user  Int  Auto_increment  NOT NULL ,
-        mail     Varchar (50) NOT NULL ,
-        password Varchar (50) NOT NULL
-	,CONSTRAINT User_PK PRIMARY KEY (id_user)
-)ENGINE=InnoDB;
+	id_user    SERIAL NOT NULL ,
+	mail       VARCHAR (50) NOT NULL ,
+	password   VARCHAR (50) NOT NULL  ,
+	CONSTRAINT User_PK PRIMARY KEY (id_user)
+)WITHOUT OIDS;
 
 
-#------------------------------------------------------------
-# Table: State
-#------------------------------------------------------------
-
+------------------------------------------------------------
+-- Table: State
+------------------------------------------------------------
 CREATE TABLE iwa.State(
-        id_state    Int  Auto_increment  NOT NULL ,
-        label_state Varchar (50) NOT NULL
-	,CONSTRAINT State_PK PRIMARY KEY (id_state)
-)ENGINE=InnoDB;
+	id_state      SERIAL NOT NULL ,
+	label_state   VARCHAR (50) NOT NULL  ,
+	CONSTRAINT State_PK PRIMARY KEY (id_state)
+)WITHOUT OIDS;
 
 
-#------------------------------------------------------------
-# Table: Location
-#------------------------------------------------------------
-
+------------------------------------------------------------
+-- Table: Location
+------------------------------------------------------------
 CREATE TABLE iwa.Location(
-        id_location Int  Auto_increment  NOT NULL ,
-        longitute   Int NOT NULL ,
-        latitude    Int NOT NULL
-	,CONSTRAINT Location_PK PRIMARY KEY (id_location)
-)ENGINE=InnoDB;
+	id_location   SERIAL NOT NULL ,
+	longitute     INT  NOT NULL ,
+	latitude      INT  NOT NULL  ,
+	CONSTRAINT Location_PK PRIMARY KEY (id_location)
+)WITHOUT OIDS;
 
 
-#------------------------------------------------------------
-# Table: Notification
-#------------------------------------------------------------
-
+------------------------------------------------------------
+-- Table: Notification
+------------------------------------------------------------
 CREATE TABLE iwa.Notification(
-        id_notification    Int  Auto_increment  NOT NULL ,
-        date_notification  TimeStamp NOT NULL ,
-        label_notification Varchar (50) NOT NULL ,
-        id_location        Int NOT NULL ,
-        id_user            Int NOT NULL ,
-        id_state           Int NOT NULL
-	,CONSTRAINT Notification_PK PRIMARY KEY (id_notification)
+	id_notification      SERIAL NOT NULL ,
+	date_notification    TIMESTAMP  NOT NULL ,
+	label_notification   VARCHAR (50) NOT NULL ,
+	id_location          INT  NOT NULL ,
+	id_user              INT  NOT NULL ,
+	id_state             INT  NOT NULL  ,
+	CONSTRAINT Notification_PK PRIMARY KEY (id_notification)
 
-	,CONSTRAINT Notification_Location_FK FOREIGN KEY (id_location) REFERENCES Location(id_location)
-	,CONSTRAINT Notification_User0_FK FOREIGN KEY (id_user) REFERENCES User(id_user)
-	,CONSTRAINT Notification_State1_FK FOREIGN KEY (id_state) REFERENCES State(id_state)
-)ENGINE=InnoDB;
+	,CONSTRAINT Notification_Location_FK FOREIGN KEY (id_location) REFERENCES iwa.Location(id_location)
+	,CONSTRAINT Notification_User0_FK FOREIGN KEY (id_user) REFERENCES iwa.User(id_user)
+	,CONSTRAINT Notification_State1_FK FOREIGN KEY (id_state) REFERENCES iwa.State(id_state)
+)WITHOUT OIDS;
 
 
-#------------------------------------------------------------
-# Table: user_state
-#------------------------------------------------------------
-
+------------------------------------------------------------
+-- Table: user_state
+------------------------------------------------------------
 CREATE TABLE iwa.user_state(
-        id_state Int NOT NULL ,
-        id_user  Int NOT NULL ,
-        date     TimeStamp NOT NULL
-	,CONSTRAINT user_state_PK PRIMARY KEY (id_state,id_user)
+	id_state   INT  NOT NULL ,
+	id_user    INT  NOT NULL ,
+	date       TIMESTAMP  NOT NULL  ,
+	CONSTRAINT user_state_PK PRIMARY KEY (id_state,id_user)
 
-	,CONSTRAINT user_state_State_FK FOREIGN KEY (id_state) REFERENCES State(id_state)
-	,CONSTRAINT user_state_User0_FK FOREIGN KEY (id_user) REFERENCES User(id_user)
-)ENGINE=InnoDB;
+	,CONSTRAINT user_state_State_FK FOREIGN KEY (id_state) REFERENCES iwa.State(id_state)
+	,CONSTRAINT user_state_User0_FK FOREIGN KEY (id_user) REFERENCES iwa.User(id_user)
+)WITHOUT OIDS;
 
 
-#------------------------------------------------------------
-# Table: user_localized
-#------------------------------------------------------------
-
+------------------------------------------------------------
+-- Table: user_localized
+------------------------------------------------------------
 CREATE TABLE iwa.user_localized(
-        id_location Int NOT NULL ,
-        id_user     Int NOT NULL ,
-        date        TimeStamp NOT NULL
-	,CONSTRAINT user_localized_PK PRIMARY KEY (id_location,id_user)
+	id_location   INT  NOT NULL ,
+	id_user       INT  NOT NULL ,
+	date          TIMESTAMP  NOT NULL  ,
+	CONSTRAINT user_localized_PK PRIMARY KEY (id_location,id_user)
 
-	,CONSTRAINT user_localized_Location_FK FOREIGN KEY (id_location) REFERENCES Location(id_location)
-	,CONSTRAINT user_localized_User0_FK FOREIGN KEY (id_user) REFERENCES User(id_user)
-)ENGINE=InnoDB;
+	,CONSTRAINT user_localized_Location_FK FOREIGN KEY (id_location) REFERENCES iwa.Location(id_location)
+	,CONSTRAINT user_localized_User0_FK FOREIGN KEY (id_user) REFERENCES iwa.User(id_user)
+)WITHOUT OIDS;
+
+
+
+
 
