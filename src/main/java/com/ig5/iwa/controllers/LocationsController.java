4ae5a73e09bee7790b4530801558fca6ff7c8401
Polyrayan <32355939+Polyrayan@users.noreply.github.com>
2020-11-24 16:07:42 +0100

    endpoints carré

--- src/main/java/com/ig5/iwa/controllers/LocationsController.java
+++ src/main/java/com/ig5/iwa/controllers/LocationsController.java
@@ -1,7 +1,6 @@
 package com.ig5.iwa.controllers;
 
 import com.ig5.iwa.models.Location;
-import com.ig5.iwa.models.User;
 import com.ig5.iwa.models.User_Localized;
 import com.ig5.iwa.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.persistence.GeneratedValue;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -49,4 +47,8 @@ public class LocationsController {
         return locationRepository.saveAndFlush(sendNewLoc);
     }
 
+    @RequestMapping(path= "exists", method = RequestMethod.POST)
+    public Boolean exists(@RequestBody Location location){
+        return locationRepository.findAll().stream().filter(l-> (l.getLongitude() == location.getLongitude()) && (l.getLatitude() == location.getLatitude())).count()>0;
+    }
 }
--- src/main/java/com/ig5/iwa/controllers/UserLocalizedController.java
+++ src/main/java/com/ig5/iwa/controllers/UserLocalizedController.java
@@ -1,18 +1,20 @@
 package com.ig5.iwa.controllers;
 
 import com.ig5.iwa.models.Location;
+import com.ig5.iwa.models.User;
 import com.ig5.iwa.models.User_Localized;
+import com.ig5.iwa.repositories.LocationRepository;
 import com.ig5.iwa.repositories.UserLocalizedRepository;
+import com.ig5.iwa.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @RequestMapping("api/v1/user_localized")
 @RestController
@@ -20,6 +22,11 @@ public class UserLocalizedController {
     @Autowired
     public UserLocalizedRepository userLocalizedRepository;
 
+    @Autowired
+    public LocationRepository locationRepository;
+
+    @Autowired
+    public UserRepository userRepository;
 
     @GetMapping
     public List<User_Localized> list() {
@@ -38,4 +45,20 @@ public class UserLocalizedController {
             return currentLoc;
         }
     }
+
+    @PostMapping(value = "add/{id_user}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public User addUserLocalized(@PathVariable int id_user, @RequestBody Location data) {
+        if(userRepository.findById(id_user).isPresent()){
+            User u = userRepository.findById(id_user).orElse(new User());
+            Location location = new Location(data.getLongitude(),data.getLatitude());
+            Location l = locationRepository.save(location);
+            System.out.println("------------  loc "+l.getId_Location());
+            User_Localized ul = new User_Localized(u,l);
+            u.addUserLocation(ul);
+            return userRepository.saveAndFlush(u);
+        }
+        return null;
+    }
+
 }
