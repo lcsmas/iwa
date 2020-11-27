d129871844e15bc96f875f67e61840a2f4a1c484
Polyrayan <32355939+Polyrayan@users.noreply.github.com>
2020-11-27 13:05:28 +0100

    add service getIdStateOfUserState

--- src/main/java/com/ig5/iwa/services/UserStateService.java
+++ src/main/java/com/ig5/iwa/services/UserStateService.java
@@ -2,7 +2,6 @@ package com.ig5.iwa.services;
 
 
 import com.ig5.iwa.models.*;
-import com.ig5.iwa.repositories.UserLocalizedRepository;
 import com.ig5.iwa.repositories.UserStateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,6 +34,10 @@ public class UserStateService {
         return userStateRepository.findTopById_IdUserOrderByDateDesc(id_user).map(User_State::getState).map(State::getLabel_state);
     }
 
+    public Optional<Integer> getIdStateOfUserState(int id_user){
+        return userStateRepository.findTopById_IdUserOrderByDateDesc(id_user).map(User_State::getState).map(State::getId_state);
+    }
+
     public Boolean findOneUserStateWithUserId(int id_user){
         return userStateRepository.findTopById_IdUserOrderByDateDesc(id_user).isEmpty();
     }
@@ -44,15 +47,6 @@ public class UserStateService {
     }
 
         public User saveAndFlush(int id_user, String state_label) {
-        /*
-        User u = userService.findUserById(id_user).orElse(new User());
-        State state = new State(state_label);
-        State stateSave = stateService.save(state);
-        User_State us = new User_State(u,stateSave);
-        u.addUserState(us);
-        return userService.saveAndFlush(u);
-
-         */
         User u = userService.findUserById(id_user).orElse(new User());
         State state = new State(state_label);
         State stateSave = stateService.save(state);
